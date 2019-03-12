ad206c2019ce31214fc8bb4388034ed78dae126d
huanglei2 <huanglei2@jiangroom.com>
2019-03-12 17:25:26 +0800

    first commit

---
+++ jiangroom/zuul-server/src/main/java/com/jiang/room/zuul/filter/UserFilter.java
@@ -0,0 +1,28 @@
+package com.jiang.room.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
