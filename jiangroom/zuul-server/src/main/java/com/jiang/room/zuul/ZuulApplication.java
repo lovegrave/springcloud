1d5b6c52a7dbdf5babef185bb616b33566b34324
huanglei2 <huanglei2@jiangroom.com>
2019-03-13 11:12:05 +0800

    oauth

--- jiangroom/zuul-server/src/main/java/com/jiang/room/zuul/ZuulApplication.java
+++ jiangroom/zuul-server/src/main/java/com/jiang/room/zuul/ZuulApplication.java
@@ -2,16 +2,33 @@ package com.jiang.room.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class ZuulApplication {
+@EnableOAuth2Sso
+public class ZuulApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+         .authorizeRequests()
+         .antMatchers("/login","/client/**")
+         .permitAll()
+         .anyRequest()
+         .authenticated()
+         .and()
+         .csrf()
+         .disable();
     }
 
 }
